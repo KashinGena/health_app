4f26c4cb75c7e619bfb6f7908800f92e8d5ed4f4
Геннадий Кашин <gennady.kashin94@gmail.com>
2021-09-12 17:47:17 +0300

    Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.scss'
+import logo from '../../assets/logo.png'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="header__inner">
+                <div className="header__logo">
+                    <img alt='logo' src={logo} className="header__logo-img"/>
+                </div>
+                <div className="header__container">
+                    <nav className="menu">
+                        <ul className="menu__list">
+                            <li className="menu__list-item">
+                                Контакты
+                            </li>
+                        </ul>
+                    </nav>
+                    <button className='header__login-btn'>Войти</button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
